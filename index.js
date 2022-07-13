17f34f19b0756e3dd6a3543e5e08d72333c3a1ab
Luong Thien Phuoc <phuoc.t.luong@taptap.com.vn>
2022-07-13 17:24:45 +0700

    done responsive

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ console.log(elementHeader);
 console.log(elementHeader.scrollTop + elementHeader.scrollHeight);
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
   if (
     window.scrollY >=
     elementIntroduce.scrollTop + elementIntroduce.scrollHeight
@@ -15,3 +14,22 @@ window.addEventListener("scroll", () => {
     elementHeader.classList.remove("header-fixed");
   }
 });
+
+const btnMenu = document.querySelector(".btn-menu-tablet");
+const elementModalMobile = document.querySelector(".modal-header-mobile");
+btnMenu.addEventListener("click", () => {
+  elementModalMobile.classList.remove("hide");
+});
+
+const btnOut = document.querySelector(".btn-out");
+btnOut.addEventListener("click", () => {
+  elementModalMobile.classList.add("hide");
+});
+
+const listBtnMobile = document.querySelectorAll(".nav-item-mobile");
+
+for (let i = 0; i < listBtnMobile.length; i++) {
+  listBtnMobile[i].addEventListener("click", () => {
+    elementModalMobile.classList.add("hide");
+  });
+}
